3ebed9be5cb54847db3053ba083bfb2df0cd37c2
Darryl Blake <darryl@darrylblake.com>
2024-06-28 20:08:27 +1200

    chore: don't make scss default and remove unneeded title/component from storybook export (#2)

--- ExampleComponent/ExampleComponent.stories.tsx
+++ ExampleComponent/ExampleComponent.stories.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { Meta } from "@storybook/react";
 import { ExampleComponent } from "./ExampleComponent";
 
-export default {
-  title: "ExampleComponent",
-  component: ExampleComponent,
-} as Meta;
-
 export const Base = () => <ExampleComponent>ExampleComponent</ExampleComponent>;
+
+export default {};
--- ExampleComponent/ExampleComponent.tsx
+++ ExampleComponent/ExampleComponent.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import style from "./ExampleComponent.module.scss";
 
 type ExampleComponentProps = {
   children: React.ReactNode;
 };
 
 export const ExampleComponent = ({ children }: ExampleComponentProps) => {
-  return <div className={style.container}>{children}</div>;
+  return <div>{children}</div>;
 };
