47db56ed6bdeb32eca011b348bd97268913aae38
Darryl Blake <darryl@darrylblake.com>
2024-06-28 20:10:31 +1200

    chore: include scss output in 'ExampleComponent'

--- ExampleComponent/ExampleComponent.tsx
+++ ExampleComponent/ExampleComponent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "./ExampleComponent.module.scss";
 
 type ExampleComponentProps = {
   children: React.ReactNode;
 };
 
 export const ExampleComponent = ({ children }: ExampleComponentProps) => {
-  return <div>{children}</div>;
+  return <div className={style.container}>{children}</div>;
 };
